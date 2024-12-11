3d55a46feb61cd60abea7051b5e97e1ccdac9c26
Jan Almar (Akagi) Orfanel <71751330+AsuzagawaAkagi@users.noreply.github.com>
2024-12-11 17:59:45 +0800

    Add files via upload

---
+++ src/components/ConductorPassengerControl.tsx
@@ -0,0 +1,73 @@
+import { actions } from "astro:actions"
+import { useState, useMemo } from "react"
+
+
+export const ConductorPassengerControl = ({ initialCount, tripId, tripStatus }: { initialCount: number, tripId: string, tripStatus: 'not_started' | 'in_progress' | 'completed' }) => {
+
+    const [passengerCount, setPassengerCount] = useState(initialCount)
+    const [input, setInput] = useState(0)
+
+    const handleChange = async ({ type, count }: { type: 'add' | 'sub', count: number }) => {
+        try {
+            await actions.changePassengerCount({
+                type,
+                count,
+                tripId
+            })
+            location.reload()
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+
+    return (
+        <div className="border-4 flex-grow border-black bg-white p-6">
+            <h2 className="text-3xl font-bold uppercase border-b-4 border-black pb-2">
+                Current Passengers Control
+            </h2>
+            <div className="mt-4">
+                <div className="border-2 border-black p-4 bg-gray-100">
+                    <p className="font-mono text-3xl mb-4">
+                        Count: {passengerCount}
+                    </p>
+                    {tripStatus === "in_progress" ? (<>
+                        <input value={input} onChange={(e) => setInput(Number(e.target.value))} type="number" className="border-2 px-4 py-2 h-12 mb-4" />
+                        <div className="w-full flex gap-4">
+                            <button
+                                onClick={() => {
+                                    if (input === 0) return;
+                                    if (window.confirm(`Are you sure you want to add ${input} passengers?`)) {
+                                        handleChange({ type: 'add', count: input })
+                                    }
+                                }}
+                                className="mt-2 border-2 flex-grow border-black px-4 py-2 font-mono hover:bg-black hover:text-white transition-colors"
+                            >
+                                + Add {input}
+                            </button>
+                            <button
+                                onClick={() => {
+                                    if (input === 0) return;
+                                    const newCount = passengerCount - input;
+                                    if (newCount >= 0 && window.confirm(`Are you sure you want to subtract ${input} passengers?`)) {
+                                        handleChange({ type: 'sub', count: input })
+                                    } else if (newCount < 0) {
+                                        alert("Passenger count cannot be negative!");
+                                    }
+                                }}
+                                className="mt-2 border-2 flex-grow border-black px-4 py-2 font-mono hover:bg-black hover:text-white transition-colors"
+                            >
+                                - Subtract {input}
+                            </button>
+                        </div>
+                    </>
+                    ) : (
+                        <p className="text-sm text-gray-600">
+                            {tripStatus === "not_started" ? "Trip still not started! Please start the trip first." : "Trip already completed!"}
+                        </p>
+                    )}
+                </div>
+            </div>
+        </div>
+    )
+}
