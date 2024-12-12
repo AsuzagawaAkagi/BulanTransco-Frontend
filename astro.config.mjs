194ab1473619c94dc019888e423fa03d8534ecd4
Jan Almar (Akagi) Orfanel <71751330+AsuzagawaAkagi@users.noreply.github.com>
2024-12-12 08:30:08 +0800

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,17 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
+
 import node from "@astrojs/node";
+
+import metaTags from "astro-meta-tags";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sentry(), spotlightjs(), metaTags()],
   output: "server",
   prefetch: false,
   adapter: node({
